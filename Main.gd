59c78be7537f48fcdd0e34eca35cb98199918742
Dan Gaylord <danielgaylord@gmail.com>
2022-01-18 00:39:56 -0500

    Final updates

--- Main.gd
+++ Main.gd
@@ -14,10 +14,12 @@ func _ready():
 #	pass
 
 func game_over():
+	get_tree().call_group("mobs", "queue_free")
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
-	get_tree().call_group("mobs", "queue_free")
+	$DeathSound.play()
+	$Music.stop()
 
 func new_game():
 	score = 0
@@ -25,17 +27,22 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_MobTimer_timeout():
-	$MobPath/MobSpawnLocation.offset = randi()
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.offset = randi()
+	
 	var mob = Mob.instance()
 	add_child(mob)
-	var direction = $MobPath/MobSpawnLocation.rotation + (PI / 2)
-	mob.position = $MobPath/MobSpawnLocation.position
+	
+	var direction = mob_spawn_location.rotation + (PI / 2)
+	mob.position = mob_spawn_location.position
 	direction += rand_range(-PI / 4, PI / 4)
 	mob.rotation = direction
-	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
+	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = velocity.rotated(direction)
 
 func _on_ScoreTimer_timeout():
 	score += 1
